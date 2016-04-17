c66d7685ee3802a8fba66d664cce2e0bf842d45a
Michael Sarchet <mikesarchet@gmail.com>
2016-04-17 09:36:29 -0700

    Start of handlers

---
+++ src/website/js/handlers/messageHandlers.js
@@ -0,0 +1,12 @@
+export class MessageHandlers {
+  constructor(message) {
+    this.message = message;
+    this.handlers = [
+
+    ];
+  }
+
+  parse() {
+    this.messageType = message.type
+  }
+}
