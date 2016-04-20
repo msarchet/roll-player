6c085b0472dc379f3a62b0a3a5b9e89a87d779f6
Michael Sarchet <mikesarchet@gmail.com>
2016-04-20 12:58:04 -0700

    Don't build to a src folder

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,15 @@
 const gulp = require('gulp');
 const webpack = require('gulp-webpack');
 const jade = require('gulp-jade');
+
 gulp.task('webpack', () => {
   return gulp.src('./src/website/js/main.js')
     .pipe(webpack(require('./webpack.config.js')))
-    .pipe(gulp.dest('./build/src/website'));
+    .pipe(gulp.dest('./build/website'));
 });
 
 gulp.task('jade', () => {
-  return gulp.src('./src/webpack/jade/**/*.jade')
+  return gulp.src('./src/website/html/**/*.jade')
     .pipe(jade({}))
-    .pipe(gulp.dest('./build/src/website/html'));
+    .pipe(gulp.dest('./build/website/html'));
 });
