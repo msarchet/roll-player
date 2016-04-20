86642e4e4e95188947afef353ebcf8147b51b176
Michael Sarchet <mikesarchet@gmail.com>
2016-04-19 18:03:59 -0700

    Use a shunting yard algorithm to parse the input and convert to rolls

--- src/roller/index.js
+++ src/roller/index.js
@@ -1,75 +1,238 @@
 'use strict';
 const random = require('random-js');
-const standard = /((\d*)d(\d+))(k(\d+))*([\+-]\d+)*/;
 const engine = random.engines.mt19937().autoSeed();
-
-let parse = (dieString, cb) => {
-  if(standard.test(dieString)) {
-    let result = standard.exec(dieString);
-    let dice = makeDice(result);
-    let keep = parseInt(result[5]);
-    if(keep > dice.length) {
-      return cb(new Error('trying to keep more dice than you are rolling'));
+const operatorPrecedence = {
+  'd': 0,
+  'k': 0,
+  'r': 0, 
+  'ro': 0,
+  'r<': 0,
+  'r>': 0,
+  '+': 1,
+  '-': 1
+}
+const parse = (roll, callback) => {
+  let tokenized = roll.split('');
+  // rejoin numbers
+  let index = 0;
+  let joined = [];
+  let current = [];
+  while(index < tokenized.length) {
+    if(!isOperator(tokenized[index])) {
+      current.push(tokenized[index]);
+    } else {
+      if(current.length > 0) {
+        let join = current.join('');
+        current = [];
+        joined.push(join);
+      }
+      let token = tokenized[index];
+      if(token == 'r') {
+        let next = tokenized[index + 1];
+        console.log('next character', next, tokenized);
+        if(next == 'o' || next == '<' || next == '>') {
+          console.log('moving next over', next);
+          token += next;
+          index++;
+        }
+      }
+      console.log('token is', token);
+      joined.push(token);
     }
-    let modifier = checkModifier(result[6]);
-    return cb(null, {dice, modifier, keep});
+    index++;
+  }
+  if(current.length > 0) {
+    let join = current.join('');
+    current = [];
+    joined.push(join);
+  }
+  tokenized = joined;
+  let token = null; 
+  let output = [];
+  let operators = [];
+  // start construction of the tree
+  while(token = tokenized.shift()) {
+    if(isOperator(token)) {
+      let top = operators[operators.length - 1];
+      if(top != null && operatorPrecedence[top] <= operatorPrecedence[token]) {
+        output.push(operators.pop());
+      }
+      operators.push(token);
+      output.push(tokenized.shift() || 1);
+    } else {
+      output.push(token);
+    }
   }
 
-  return cb(new Error('unable to parse input'));
+  while(token = operators.pop()) {
+    output.push(token);
+  }
+  
+  callback(null, output);
 }
 
-let checkModifier = (mod) => {
-  return parseInt(mod || 0);
+const roll = (rpn, callback) => {
+  let value = evaluate(rpn);
+  callback(null, value);
 };
-let makeDice = (parsed) => {
-  let count = parseInt(parsed[2] === '' ? '1' : parsed[1]);
-  let die = parseInt(parsed[3]);
-  let dice = [];
-  for(var x = 0; x < count; x++) {
-    dice.push({die: {sides: die}});
+
+const evaluate = rpn => {
+  let operator = null;
+  let index = 0; 
+  let token = rpn[index];
+  while(!isOperator(token)) {
+    index++; 
+    token = rpn[index];
+    if(token == undefined) {
+      break;
+    }
   }
-  return dice;
+
+  let left = rpn[index - 2];
+  let right = rpn[index - 1];
+  let value = null;
+  switch(token) {
+    case 'd':
+      value = createDie(left, right);
+      break;
+    case 'k':
+      value = createKeep(left, right);
+      break;
+    case '+':
+    case '-':
+      value = createMath(left, right, token);
+      break;
+    case 'r':
+    case 'r<':
+    case 'r>':
+    case 'ro':
+      value = reroll(left, right, token);
+      break;
+  }
+
+  let start = index - 2;
+  start = start < 0 ? 0 : start;
+  let countToRemove = index - start + 1;
+  rpn.splice(start, countToRemove, value);
+  if(rpn.length > 1) {
+    return evaluate(rpn);
+  } else {
+    return rpn[0];
+  }
 }
 
-let roll = (parsed, cb) => {
-  let rolled = parsed.dice.map((dieObj) => {
-    let die = dieObj.die;
-    let value = random.die(die.sides)(engine);
-    die.value = value;
-    return {die};
+const sum = values => {
+  return values.reduce((i, v) => i += v.roll, 0);
+}
+
+const createDie = (number, sides) => {
+  number = number || 1;
+  let values = random.dice(sides, number)(engine).map(r => {
+    return {roll: r}
   });
-
-  parsed.dice = rolled;
-  parsed.rolled = true;
-
-  // reroll
-
-  // keep|drop
-  if(parsed.keep) {
-    if(parsed.keep > parsed.dice.length) {
-      return cb(new Error('Trying to keep more dice than rolled'));
-    }
-    parsed.kept = [];
-    rolled = rolled.sort(function(a, b) {
-      return a.die.value > b.die.value ? -1 : 1;
-    });
-    for(var x = 0; x < parsed.keep; x++) {
-      parsed.kept.push(parsed.dice[x]);
-    }
+  return {
+    type: 'roll',
+    isRoll: true,
+    number,
+    sides,
+    values, 
+    value: () => sum(values)
   }
-
-  parsed.beforeModifier = sum(parsed.kept || parsed.dice);
-  parsed.finalValue = parsed.beforeModifier + parsed.modifier;
-  return cb(null, parsed);
 }
 
-let sum = (dice) => {
-  return dice.reduce((i, dieObj) => {
-    return i += dieObj.die.value;
-  }, 0);
+const createKeep = (roll, keep) => {
+  let values = roll.values.sort((a,b) => {
+    return a.roll > b.roll ? -1 : 1; 
+  }).slice(0, keep);
+  return {
+    type: 'keptDice',
+    isRoll: true,
+    number: keep,
+    values,
+    originalRoll: roll,
+    value: () => sum(values) 
+  }
+}
+
+const createMath = (roll, modifier, operation) => {
+  return {
+    type: 'modifiedRoll',
+    isRoll: false,
+    number: roll.length,
+    modifier: modifier,
+    operation: operation,
+    originalRoll: roll,
+    value: () => {
+      let modifierValue = modifier.value ? modifier.value() : parseInt(modifier);
+      if(operation == '+') {
+        return roll.value() + modifierValue;
+      } else if(operation == '-'){
+        return roll.value() - modifierValue;
+      }
+    }
+  }
+}
+const shouldReroll = (value, target, type) => {
+  switch(type) {
+    case 'ro': 
+    case 'r':
+      console.log('value|target', value, '|', target);
+      return value == target;
+    case 'r<':
+      return value <= target;
+    case 'r>':
+      return value >= target;
+  }
+}
+const reroll = (roll, target, token) => {
+  if(!roll.isRoll) {
+    throw new Error('Can\'t reroll a non-roll');
+  }
+
+  let targets = roll.targets || [];
+  targets.push(target);
+  let values = [];
+  if(token == 'ro') {
+    values = roll.values.map((value) => {
+      if(shouldReroll(value.roll, target, token)) {
+        let newRoll = random.die(roll.sides)(engine);
+        return {roll: newRoll, original: value}; 
+      }
+
+      return value;
+    });
+  } else if(token == 'r<' || token == 'r>' || token == 'r') {
+    values = roll.values.map((value) => {
+      let newRoll = 0;
+      let rerollCount = 0;
+      while(shouldReroll(newRoll || value.roll, target, token)) {
+        newRoll = random.die(roll.sides)(engine);
+        rerollCount++;
+      }
+      if(rerollCount > 0) {
+        return {roll: newRoll, original: value, rerollCount}; 
+      }
+      return value;
+    });
+  }
+  return {
+    type: 'rerollRoll',
+    isRoll: true,
+    originalRoll: roll,
+    targets,
+    values,
+    number: roll.number,
+    sides: roll.sides,
+    value: () => sum(values)
+  }
+}
+
+const isOperator = token => {
+  return Object.keys(operatorPrecedence).indexOf(token) !== -1
 }
 
 module.exports = {
   parse,
-  roll
+  roll,
+  sum
 }
--- tests/roller/parsing.spec.js
+++ tests/roller/parsing.spec.js
@@ -7,49 +7,60 @@ describe('Die parsing', () => {
     it('should return the number of dice', (done) => {
       let parsed = roller.parse('2d6', (err, parsed) => {
         expect(err).to.equal(null);
-        expect(parsed.dice).to.have.length(2);
-        expect(parsed.dice[0]).to.have.property('die').with.property('sides').to.equal(6);
-        expect(parsed.dice[1]).to.have.property('die').with.property('sides').to.equal(6);
+        expect(parsed).to.deep.equal(['2', '6', 'd']);
         done();
       });
     });
 
     it('should return one die if no dice count is passed', () => {
       let parsed = roller.parse('d6', (err, parsed) => {
-        expect(parsed.dice).to.have.length(1);
+        expect(parsed).to.deep.equal(['6', 'd']);
       });
     });
   });
   describe('With a modifier', () => {
     it('should handle a positive modifier', done => {
       let parsed = roller.parse('2d6+1', (err, parsed) => {
-        expect(parsed.modifier).to.equal(1);
+        expect(parsed).to.deep.equal(['2', '6', 'd', '1', '+']);
         done();
       });
     });
     it('should handle a negative modifier', done => {
       let parsed = roller.parse('2d6-1', (err, parsed) => {
-        expect(parsed.modifier).to.equal(-1);
+        expect(parsed).to.deep.equal(['2', '6', 'd', '1', '-']);
+        done();
+      });
+    });
+
+    it('should handle one time rerolls', done=> {
+      roller.parse('2d6ro1', (err, parsed) => {
+        expect(parsed).to.deep.equal(['2', '6', 'd', '1', 'ro']);
+        done();
+      });
+    });
+
+    it('should handle multidigit rolls', done => {
+      roller.parse('2d20', (err, parsed) => {
+        expect(parsed).to.deep.equal(['2', '20', 'd']);
+        done();
+      });
+    });
+
+    it('shoudld handle multidigit complex rolls', done => {
+      roller.parse('20d20k10+2', (err, parsed) => {
+        expect(parsed).to.deep.equal(['20', '20', 'd', '10', 'k', '2', '+']);
         done();
       });
     });
   });
 
   describe('with a keep', () => {
-    it('should set the keep to the proper count', done => {
+    it('should handle keeps', done => {
       roller.parse('4d6k3', (err, parsed) => {
-        expect(err).to.equal(null);
-        expect(parsed).to.have.property('keep');
+        expect(parsed).to.deep.equal(['4', '6', 'd', '3', 'k']);
         done();
       });
     })
-
-    it('should error if you try to keep more dice than you roll', done => {
-      roller.parse('3d6k4', (err) => {
-        expect(err).to.be.a('Error');
-        done();
-      });
-    });
   });
 });
 
@@ -59,21 +70,21 @@ describe('Die Rolling', () => {
       let parsed = roller.parse('2d6', (err, parsed) => {
         expect(err).to.equal(null);
         roller.roll(parsed, (_err, rolled) => {
-          expect(rolled.dice).to.have.length(2);
-          expect(rolled).to.have.property('rolled').to.equal(true);
-          expect(rolled.dice[0]).to.have.property('die').with.property('value');
-          expect(rolled.dice[1]).to.have.property('die').with.property('value');
+          expect(rolled.type).to.equal('roll');
+          expect(rolled.number).to.equal('2');
+          expect(rolled.sides).to.equal('6');
+          expect(rolled.values).to.have.property('length').to.equal(2);
           done();
         });
       });
     });
 
+
     it('should adjust the roll with any modifiers', done => {
       roller.parse('1d1+1', (err, parsed) => {
         expect(err).to.equal(null);
         roller.roll(parsed, (_err, rolled) => {
-          expect(rolled.beforeModifier).to.equal(1);
-          expect(rolled.finalValue).to.equal(2);
+          expect(rolled.value()).to.equal(2);
           done();
         });
       });
@@ -83,7 +94,7 @@ describe('Die Rolling', () => {
       it('should keep the approriate number of rolls', done => {
         roller.parse('4d1k3', (err, parsed) => {
           roller.roll(parsed, (_err, rolled) => {
-            expect(rolled.finalValue).to.equal(3);
+            expect(rolled.value()).to.equal(3);
             done();
           });
         });
@@ -92,8 +103,10 @@ describe('Die Rolling', () => {
       it('should keep the correct rolls', done => {
         roller.parse('4d6k3', (err, parsed) => {
           roller.roll(parsed, (_err, rolled) => {
-            rolled.kept.forEach((keptDie) => {
-              expect(rolled.dice[3].die.value <= keptDie.die.value).to.equal(true);
+            // find the 'lowest' value from the original Roll
+            let orginalRolls = rolled.originalRoll.values.sort((a,b) => a > b ? -1 : 1);
+            rolled.values.forEach((keptValue) => {
+              expect(keptValue >= orginalRolls[3]).to.equal(true); 
             });
             done();
           });
@@ -101,15 +114,53 @@ describe('Die Rolling', () => {
       });
     });
 
+    describe('reroll', () => {
+      it('should reroll any die with the provided target', done => {
+        roller.parse('10d6r1', (err, parsed) => {
+          roller.roll(parsed, (_err, rolled) => {
+            console.log('values for 1', rolled.values);
+            done();
+          });
+        });
+      });
+      it('should reroll once any die with the provided target', done => {
+        roller.parse('10d6ro1', (err, parsed) => {
+          roller.roll(parsed, (_err, rolled) => {
+            console.log('values for one reroll', rolled.values);
+            done();
+          });
+        });
+      });
+      it('should be able to reroll multiple times', done => {
+        roller.parse('10d6r1r2r3', (err, parsed) => {
+          roller.roll(parsed, (_err, rolled) => {
+            console.log('values', rolled.values);
+            done();
+          });
+        });
+      });
+    })
     describe('combined', () => {
       it('should add modifiers to kept rolls', done => {
         roller.parse('4d6k3+1', (err, parsed) => {
           roller.roll(parsed, (_err, rolled) => {
-            expect(rolled.beforeModifier).to.equal(rolled.finalValue - 1);
+            expect(rolled.originalRoll.value()).to.equal(rolled.value() - 1);
             done();
           }); 
         });
       });
+
+      it('should handle a complex roll', done => {
+        roller.parse('9d9+4d6k3+2', (err, parsed) => {
+          roller.roll(parsed, (_err, rolled) => {
+            let final = rolled.value();
+            let d9Value = rolled.originalRoll.value();
+            let d6Value = rolled.modifier.originalRoll.value();
+            expect(final).to.equal(d9Value + d6Value + 2);
+            done();
+          });
+        });
+      })
     });
   });
 });
