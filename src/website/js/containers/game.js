c97a3ff2a5c7e7499a8f495c23b5b7327c195064
Michael Sarchet <mikesarchet@gmail.com>
2016-05-29 13:13:24 -0700

    Track email clicks

--- src/website/js/containers/game.js
+++ src/website/js/containers/game.js
@@ -3,10 +3,19 @@ import styles from '../../css/game.css';
 import Header from './headers';
 import Chat from './chat';
 
+const sendEmail = () => {
+  ga('send', {
+    hitType: 'event',
+    eventCategory: 'Email',
+    eventAction: 'clicked',
+    eventLabel: 'Acquisition',
+    transport: 'beacon'
+  })
+};
 const Game = () => (
   <div className={styles.container}>
     <h1 className={styles.title}>Die Rolling Demo</h1>
-    <div style={{display: 'inline-block'}}><span>Stay informed with an </span><a href="http://eepurl.com/b3ByN1">email</a></div>
+    <div style={{display: 'inline-block'}}><span>Stay informed with an </span><a onClick={sendEmail} href="http://eepurl.com/b3ByN1">email</a></div>
     <p className={styles.helpText}> Mouse over results to see rolls</p>
     <Header />
     <Chat />
